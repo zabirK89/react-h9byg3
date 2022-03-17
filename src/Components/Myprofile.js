050723a0455b3ee248419254e2a84f74467752c1
zabirK89 <zabirk89@gmail.com>
2022-03-17 08:25:03 +0530

    1 file modified

--- src/Components/Myprofile.js
+++ src/Components/Myprofile.js
@@ -7,12 +7,16 @@ import { CardActionArea } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { getProfile } from '../Redux/Action/Loginaction';
+import { useNavigate } from "react-router-dom";
+import { LOGOUT_SUCCESS } from "../Redux/Type/logintype";
+import { AppBar, Button, CardMedia, Toolbar, Typography } from "@mui/material";
 export default function ActionAreaCard() {
   const id = useSelector((state) => state.auth.id) || 1;
   console.log('id===', id);
   const dispatch = useDispatch();
   const [profile, setprofile] = useState({});
   const users = useSelector((state) => state.users);
+  const navigate=useNavigate()
   useEffect(() => {
     async function _GET_PROFILE() {
       try {
@@ -25,8 +29,30 @@ export default function ActionAreaCard() {
     }
     _GET_PROFILE();
   }, []);
+  const onLogout = (evt) => {
+    localStorage.removeItem("authToken");
+    dispatch({ type: LOGOUT_SUCCESS });
+    navigate("/login");
+  };
+  function AllProfile(){
+navigate("/allprofile")
+  }
   return (
     <>
+     <AppBar position="static" >
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+           My Profile
+          </Typography>
+          <Button color="inherit" onClick={AllProfile}>
+          All Profile
+          </Button>
+          <Button color="inherit" onClick={onLogout}>
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <br/>
       {profile.data && (
         <Card sx={{ maxWidth: 345 }}>
           <CardActionArea>
